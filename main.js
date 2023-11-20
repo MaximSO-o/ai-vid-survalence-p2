b661c9ee80ff619cd010f3f2186687292c42d2b6
MaximSO-o <118965752+MaximSO-o@users.noreply.github.com>
2023-11-20 19:18:59 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+objects = [];
+
+status = "";
+
+function setup() {
+
+canvas = createCanvas(380, 380);
+
+canvas.position(430, 260);
+
+video = createCapture(VIDEO);
+
+video.hide();
+  
+  canvas.center();
+
+}
+
+function draw() {
+
+image(video, 0, 0, 380, 380);
+
+}
+
+function start() {
+
+Object_Detector = ml5.objectDetector("cocossd", modelLoaded);
+
+document.getElementById("status").innerHTML = "Status : Object Detecting";
+
+Object_Name = document.getElementById("Object_name").value;
+
+}
+
+function modelLoaded() {
+
+console.log("Model Loaded!");
+
+status = true;
+
+}
